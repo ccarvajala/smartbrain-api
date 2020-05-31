bfb0db285b23bef121177a29af688d9ae91a1a08
ccarvajala <carlosjcarvajala@gmail.com>
2020-05-31 15:17:52 -0400

    hopefully it works now

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const db = knex(
         connection:
         {
             connectionString: process.env.DATABASE_URL,
-            ssl: true,
+            ssl: 
+            {
+               rejectUnauthorized: false
+            }
         }
     }
 );
